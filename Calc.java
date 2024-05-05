ac56bea01827b1936adece0bfbeda63da8bfbdc7
André Zambroni <andrezambroni@gmail.com>
2024-05-05 16:12:09 -0300

    final

--- Calc.java
+++ Calc.java
@@ -1,8 +1,32 @@
+
 import java.util.Scanner;
 
 public class Calc {
 
-    Scanner scan = new Scanner(System.in);
+    static public void main(String[] args) {
 
-    
+        Scanner scan = new Scanner(System.in);
+
+        Num n1 = new Num();
+        Num n2 = new Num();
+        Num res = new Num();
+        String opc = "s";
+
+        while ("s".equals(opc) || "S".equals(opc)) {
+            System.out.printf("%nDigite o valor 1: ");
+            n1.setValor(scan.nextInt());
+
+            System.out.printf("%nDigite o valor 2: ");
+            n2.setValor(scan.nextInt());
+
+            res.setValor(n1.getValor() + n2.getValor());
+            System.err.printf("%nA some de %d com %d eh igual a %d", n1.getValor(), n2.getValor(), res.getValor());
+
+            System.err.printf("%nDeseja soma outro valor?");
+            opc=scan.next();
+            System.out.printf("%n %n %n  ");
+
+        }
+    }
+
 }
--- Num.java
+++ Num.java
@@ -1,3 +1,18 @@
 public class Num {
-    
+
+    private int valor;
+
+    public Num() {
+        this.valor = 0;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+        // o .valor recebe o int valor
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+
 }
